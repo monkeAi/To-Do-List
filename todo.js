69ddbc4c864e662046a6e80d6391859e289aad17
monkeAi <viktor.gprimozic@gmail.com>
2021-01-24 20:03:48 +0100

    first commit

---
+++ todo.js
@@ -0,0 +1,82 @@
+const ul = document.querySelector("#main-done-ul")
+const ol = document.querySelector("#main-ol");
+const submitBtn = document.getElementById("main-button");
+const submitInput = document.getElementById("main-input");
+if (!window.localStorage.getItem("tasks")) {
+    window.localStorage.setItem("tasks", JSON.stringify([]));
+}
+let tasks = JSON.parse(window.localStorage.getItem("tasks"));
+renderTasks();
+renderDoneTasks();
+
+function clicked() {
+  var inputValue = submitInput.value;
+  var trimedInput = inputValue.trim();
+  submitInput.value = null;
+  console.log(tasks);
+
+  if (trimedInput !== "") {
+      tasks.push({name:inputValue, done:false});
+      renderTasks();
+  }
+}
+function renderDoneTasks() {
+    ul.innerHTML = '';
+    tasks.forEach((task, i) => {
+        if (task.done){
+            var listItem = document.createElement("li"); 
+            var pItem = document.createElement('p');
+            var pText = document.createTextNode(task.name);
+
+            pItem.appendChild(pText);
+            listItem.appendChild(pItem);
+
+            ul.appendChild(listItem);
+        }
+    });
+}
+
+function renderTasks() {
+  ol.innerHTML = '';
+  saveTasks();
+  tasks.forEach((task, i) => {
+    if (!task.done) {
+      var listItem = document.createElement("li");
+      var pItem = document.createElement('p');
+      var pText = document.createTextNode(task.name);
+      var doneBtn = document.createElement("button");
+      var btnText = document.createTextNode("Done");
+
+      doneBtn.appendChild(btnText);
+      doneBtn.setAttribute('onclick', 'itemDone('+ i +')');
+
+      pItem.appendChild(pText);
+      listItem.appendChild(pItem);
+      listItem.appendChild(doneBtn);
+
+      ol.appendChild(listItem);   
+    }
+  });
+  renderDoneTasks();
+}
+
+function clearAll() {
+    window.localStorage.clear();
+    location.reload();
+}
+
+function itemDone(index) {
+    tasks[index].done = true;
+    renderTasks();
+}
+
+function completeAll() {
+    tasks.forEach(task => {
+        task.done = true;
+    });
+    renderTasks();
+}
+
+function saveTasks() {
+    window.localStorage.setItem('tasks', JSON.stringify(tasks));
+}
