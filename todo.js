b0dec709e179f0e9764f7ca00d0c2aaede712736
monkeAi <viktor.gprimozic@gmail.com>
2021-01-26 16:55:42 +0100

    third commit

--- todo.js
+++ todo.js
@@ -2,12 +2,18 @@ const ul = document.querySelector("#main-done-ul")
 const ol = document.querySelector("#main-ol");
 const submitBtn = document.getElementById("main-button");
 const submitInput = document.getElementById("main-input");
+
 if (!window.localStorage.getItem("tasks")) {
     window.localStorage.setItem("tasks", JSON.stringify([]));
 }
 let tasks = JSON.parse(window.localStorage.getItem("tasks"));
 renderTasks();
-renderDoneTasks();
+
+// This capitalizes the first letter, nvm doesnt work
+// const capitalize = (s) => {
+//     if (typeof s !== 'string') return ''
+//     return s.charAt(0).toUpperCase() + s.slice(1)
+// }
 
 submitInput.addEventListener("keyup", function(event) {
     if (event.keyCode == 13) {
@@ -17,15 +23,22 @@ submitInput.addEventListener("keyup", function(event) {
 })
 
 function clicked() {
-  var inputValue = submitInput.value;
-  var trimedInput = inputValue.trim();
-  submitInput.value = null;
-  
-  if (trimedInput !== "") {
-      tasks.push({name:inputValue, done:false});
-      renderTasks();
-  }
+    var inputValue = submitInput.value;
+    var trimedInput = inputValue.trim();
+    submitInput.value = null;
+      
+    if (trimedInput !== "") {
+        tasks.push({name:inputValue, done:false});
+        renderTasks();
+    }
+
+    if (inputValue === "function whatIsLove() {const love = ula + viktor; return love}") {
+        uFoundMe();
+        tasks.pop();
+        renderTasks();
+    }
 }
+
 function renderDoneTasks() {
     ul.innerHTML = '';
     tasks.forEach((task, i) => {
@@ -69,6 +82,15 @@ function renderTasks() {
 function clearAll() {
     tasks = [];
     renderTasks(); 
+}
+
+function uFoundMe() {
+    var img = document.getElementById('none').style;
+    img.display = 'block';
+}
+function wush() {
+    var img = document.getElementById('none').style;
+    img.display = 'none';
 }
 
 function itemDone(index) {
